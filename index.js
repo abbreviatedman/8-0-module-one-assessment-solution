dd3a8bf58b55e71a26e1945cc47ac92efdd637e4
Colin Jaffe <balloonasaurus@gmail.com>
2021-08-16 09:43:14 -0400

    New version.

---
+++ index.js
@@ -0,0 +1,277 @@
+/*
+  Do not change the line below. If you'd like to run code from this file, you may use the `exampleMovies` variable below to gain access to an array of movies.
+
+  Keep in mind that your functions must still have and use a parameter for accepting all movies.
+*/
+const exampleMovies = require("./movies");
+// Do not change the line above.
+
+/**
+ * getAllMovieTitles()
+ * -----------------------------
+ * Returns all of titles from an array of movies. If the inputted `movies` array is empty, return `[]`.
+ * @param {Object[]} movies - An array of movies. See the `movies.js` file for an example of this array.
+ * @returns {string[]} An array of strings, which are movie titles.
+ *
+ * EXAMPLE:
+ *  getAllMovieTitles(movies);
+ *  //> [
+      "Toy Story 4",
+      "Inside Out",
+      "Coco",
+      "Incredibles 2",
+      "Moana",
+      "How to Train Your Dragon",
+      "Paddington",
+      "The Lion King",
+      "Fantasia",
+      "James and the Giant Peach",
+    ];
+ */
+function getAllMovieTitles(movies) {
+  const result = [];
+  for (const movie of movies) {
+    result.push(movie.title);
+  }
+
+  return result;
+}
+
+/**
+ * getHighestMetascore()
+ * -----------------------------
+ * Returns the highest `metascore` among all movies. If the inputted `movies` array is empty, return `0`.
+ * @param {Object[]} movies - An array of movies. See the `movies.js` file for an example of this array.
+ * @returns {number} The highest `metascore` of all movies.
+ *
+ * EXAMPLE:
+ *  getHighestMetascore(movies);
+ *  //> 96
+ */
+function getHighestMetascore(movies) {
+  if (movies.length === 0) {
+    return 0;
+  }
+
+  let highest = Number(movies[0].metascore);
+  for (const movie of movies) {
+    const metascore = Number(movie.metascore);
+    if (metascore > highest) {
+      highest = metascore;
+    }
+  }
+
+  return highest;
+}
+
+/**
+ * getAverageIMDBRating()
+ * -----------------------------
+ * Averages all of the IMDB ratings from all movies and returns it, as a number. If the inputted `movies` array is empty, return `0`.
+ * @param {Object[]} movies - An array of movies. See the `movies.js` file for an example of this array.
+ * @returns {number} The average IMDB rating across all movies.
+ *
+ * EXAMPLE:
+ *  getAverageIMDBRating(movies);
+ *  //> 7.76
+ */
+function getAverageIMDBRating(movies) {
+  if (movies.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (const movie of movies) {
+    sum = sum + Number(movie.imdbRating);
+  }
+
+  return sum / movies.length;
+}
+
+/**
+ * countByRating()
+ * -----------------------------
+ * Returns an object where the keys are movie ratings and the values are the number of movies in the array with that rating. If the inputted `movies` array is empty, return `{}`.
+ * @param {Object[]} movies - An array of movies. See the `movies.js` file for an example of this array.
+ * @returns {Object} An object where keys are movie ratings (e.g. "PG") and the values are how many movies in the array have that rating (e.g. 7).
+ *
+ * EXAMPLE:
+ *  countByRating(movies);
+ *  //> { G: 3, PG: 7 }
+ */
+function countByRating(movies) {
+  const ratings = {};
+  for (const movie of movies) {
+    const rated = movie.rated;
+    if (rated in ratings) {
+      ratings[rated]++;
+    } else {
+      ratings[rated] = 1;
+    }
+  }
+
+  return ratings;
+}
+
+/**
+ * findById()
+ * -----------------------------
+ * Returns a movie object from an array of objects based on the ID. If the inputted `movies` array is empty or the ID does not match any movie, return `null`.
+ * @param {Object[]} movies - An array of movies. See the `movies.js` file for an example of this array.
+ * @param {string} id - A unique `imdbID`.
+ * @returns {Object|null} The movie object with the matching `imdbID`.
+ *
+ * EXAMPLE:
+ *  findById(movies, "tt1979376");
+ *  //> {
+      // Toy Story 4
+    };
+ */
+function findById(movies, id) {
+  let found = null;
+  for (const movie of movies) {
+    if (movie.imdbID === id) {
+      found = movie;
+    }
+  }
+
+  return found;
+}
+
+function findByIdAlt(movies, id) {
+  for (const movie of movies) {
+    if (movie.imdbID === id) {
+      return movie;
+    }
+  }
+
+  return null;
+}
+
+/**
+ * filterByGenre()
+ * -----------------------------
+ * Returns all movie objects with a matching genre. Case-insensitive. If the inputted `movies` array is empty or no movies match the inputted `genre`, return `[]`.
+ * @param {Object[]} movies - An array of movies. See the `movies.js` file for an example of this array.
+ * @param {string} genre - The genre of a movie. (e.g. "Fantasy")
+ * @returns {Object[]} An array of movies where at least one of the genres matches the `genre` inputted.
+ *
+ * EXAMPLE:
+ *  filterByGenre(movies, "Mystery");
+ *  //> [
+      {
+        // Coco
+      }
+    ]
+ *
+ * EXAMPLE:
+ *  filterByGenre(movies, "Horror")
+ *  //> []
+ */
+function filterByGenre(movies, genre) {
+  genre = genre.toLowerCase();
+  const genreMovies = [];
+  for (const movie of movies) {
+    const genreList = movie.genre.toLowerCase();
+    if (genreList.includes(genre)) {
+      genreMovies.push(movie);
+    }
+  }
+
+  return genreMovies;
+}
+
+/**
+ * getAllMoviesReleasedAtOrBeforeYear()
+ * -----------------------------
+ * Returns all movie objects with a `released` year equal to or less than the given year.
+ * @param {Object[]} movies - An array of movies. See the `movies.js` file for an example of this array.
+ * @param {number} year - A year as a number. (e.g. 2000)
+ * @returns {Object[]} An array of movies where the `released` year is equal to or less than the inputted year.
+ *
+ * EXAMPLE:
+ *  getAllMoviesReleasedAtOrBeforeYear(movies, 2000);
+ *  //> [
+      {
+        // The Lion King
+      },
+      {
+        // Fantasia
+      },
+      {
+        // James and the Giant Peach
+      }
+    ];
+ */
+function getAllMoviesReleasedAtOrBeforeYear(movies, year) {
+  const result = [];
+  for (const movie of movies) {
+    const imdbYear = convertDateStringToYearNumber(movie.released);
+    if (imdbYear <= year) {
+      result.push(movie);
+    }
+  }
+
+  return result;
+}
+
+function convertDateStringToYearNumber(date) {
+  const dateWords = date.split(" ");
+  const year = dateWords[dateWords.length - 1]
+
+  return Number(year);
+}
+
+/**
+ * getBiggestBoxOfficeMovie()
+ * -----------------------------
+ * Returns the name of the movie with the highest `boxOffice` amount.
+ * @param {Object[]} movies - An array of movies. See the `movies.js` file for an example of this array.
+ * @returns {string} The name of the movie that made the most money at the box office.
+ *
+ * EXAMPLE:
+ *  getBiggestBoxOfficeMovie(movies);
+ *  //> "Incredibles 2"
+ */
+function convertUserDollarStringToNumber(string) {
+  // Removes dollar sign and commas.
+  let result = '';
+  for (const char of string) {
+    if (char !== '$' && char !== ',') {
+      result = result + char;
+    }
+  }
+
+
+  return Number(result);
+}
+
+function getBiggestBoxOfficeMovie(movies) {
+  if (movies.length === 0) {
+    return null;
+  }
+
+  let highest = movies[0];
+  for (const movie of movies) {
+    const boxOffice = convertUserDollarStringToNumber(movie.boxOffice);
+    const highestBoxOffice = convertUserDollarStringToNumber(highest.boxOffice);
+
+    if (boxOffice > highestBoxOffice) {
+      highest = movie;
+    }
+  }
+
+  return highest.title;
+}
+
+// Do not change anything below this line.
+module.exports = {
+  getAllMovieTitles,
+  getHighestMetascore,
+  getAverageIMDBRating,
+  countByRating,
+  findById,
+  filterByGenre,
+  getAllMoviesReleasedAtOrBeforeYear,
+  getBiggestBoxOfficeMovie,
+};
